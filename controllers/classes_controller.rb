5fbf3d81605ae8dd20a8ce44d11ca8bf30486ccd
yossaj <yossaj@icloud.com>
2019-04-26 13:22:48 +0100

    file structure - added classes in view and added classes content

--- controllers/classes_controller.rb
+++ controllers/classes_controller.rb
@@ -5,3 +5,8 @@ require_relative( '../models/bookings.rb' )
 require_relative( '../models/classes.rb' )
 require_relative( '../models/members.rb' )
 also_reload( '../models/*' )
+
+get '/classes' do
+  @classes = FitClass.all()
+    erb(:'classes/index')
+end
