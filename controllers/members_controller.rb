e2defa89d95acc6ebc006b7824e0beccec939f37
yossaj <yossaj@icloud.com>
2019-04-26 11:50:50 +0100

    changed name of controller, added requirew

---
+++ controllers/members_controller.rb
@@ -0,0 +1,7 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/bookings.rb' )
+require_relative( '../models/classes.rb' )
+require_relative( '../models/members.rb' )
+also_reload( '../models/*' )
