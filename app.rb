ce4bb685e099d0fd818c008b8a75afd6a5bfa96f
yossaj <yossaj@icloud.com>
2019-05-05 14:50:02 +0100

    try again again

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require( 'pry-byebug' )
+# require( 'sinatra/contrib/all' )
+# require( 'pry-byebug' )
 require_relative( 'controllers/bookings_controller.rb' )
 require_relative( 'controllers/classes_controller.rb' )
 require_relative( 'controllers/members_controller.rb' )
