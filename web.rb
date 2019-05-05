92481ae9fc191fed687f3c9632617587210d6a60
yossaj <yossaj@icloud.com>
2019-05-05 13:49:20 +0100

    added to web.rb file, will it breake?

--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require_relative( 'controllers/bookings_controller.rb' )
+require_relative( 'controllers/classes_controller.rb' )
+require_relative( 'controllers/members_controller.rb' )
 
 get '/' do
     "Hello"
+    erb( :index )
 end
